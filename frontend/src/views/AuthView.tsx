99dd18b2411073c0bf80ad49b9e2935d5e1a6626
immoral-day <immoral-day@users.noreply.github.com>
2026-06-10 09:14:20 +0300

    Darken auth texture and slow motion

--- frontend/src/views/AuthView.tsx
+++ frontend/src/views/AuthView.tsx
@@ -17,8 +17,8 @@ export const AuthView: React.FC<AuthViewProps> = ({ onLogin, onRegister, loading
     const [localError, setLocalError] = useState('');
 
     const handleVideoReady = (event: React.SyntheticEvent<HTMLVideoElement>) => {
-        event.currentTarget.defaultPlaybackRate = 0.55;
-        event.currentTarget.playbackRate = 0.55;
+        event.currentTarget.defaultPlaybackRate = 0.1;
+        event.currentTarget.playbackRate = 0.1;
     };
 
     const handleSubmit = (event: React.FormEvent) => {
